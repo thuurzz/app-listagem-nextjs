b7cf61a722db2c994cd19b16b31183911a52d4fb
Arthur Vinicius <thuur.vss@gmail.com>
2022-12-25 16:25:51 -0300

    wip: form cadastra senha

--- pages/FormAdd/index.tsx
+++ pages/FormAdd/index.tsx
@@ -5,10 +5,18 @@ import {
   AlertTitle,
   Box,
   Button,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  Input,
   Modal,
+  Radio,
+  RadioGroup,
   Typography,
 } from "@mui/material";
 import { style } from "./styles";
+import { LockOpen } from "@mui/icons-material";
 
 type ISenhaPost = Omit<ISenha, "id">;
 
@@ -21,6 +29,7 @@ export const FormAdd = ({ onCloseFormAdd, openForm }: IFormAddProps) => {
   const [valor, setValor] = useState<string>("");
   const [chave, setChave] = useState<string>("");
   const [ambiente, setAmbiente] = useState<string>("");
+  const [exibir, setExibir] = useState<boolean>(false);
 
   const handleSubmit = () => {
     if (!valor || !chave || !ambiente) {
@@ -45,19 +54,60 @@ export const FormAdd = ({ onCloseFormAdd, openForm }: IFormAddProps) => {
 
   return (
     <>
-      <Modal
-        open={openForm}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
+      <Modal open={openForm} onClose={handleClose}>
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <FormGroup>
+            <Typography id="modal-modal-title" variant="h4" component="h2">
+              Cadastro de senha 🔑
+            </Typography>
+            <Typography variant="overline" sx={{ mb: "1rem" }}>
+              Armazenar no cofre de senhas
+            </Typography>
+            <FormLabel component="legend">Chave</FormLabel>
+            <Input
+              id="chave"
+              value={chave}
+              onChange={(e) => {
+                setChave(e.target.value);
+              }}
+              fullWidth
+              sx={{ mb: "1rem" }}
+            />
+            <FormLabel component="legend">Valor</FormLabel>
+            <Input
+              id="valor"
+              value={valor}
+              onChange={(e) => {
+                setValor(e.target.value);
+              }}
+              fullWidth
+              sx={{ mb: "1rem" }}
+              type={exibir ? "text" : "password"}
+            />
+            <Button
+              onClick={() => {
+                setExibir(!exibir);
+              }}
+            >
+              Exibir senha {""}
+              <LockOpen />
+            </Button>
+            <Box sx={{ mb: "2rem" }}>
+              <FormLabel component="legend">Ambiente</FormLabel>
+              <RadioGroup
+                defaultValue="dev"
+                row
+                onChange={(e) => setAmbiente(e.target.value)}
+              >
+                <FormControlLabel value="DEV" control={<Radio />} label="DEV" />
+                <FormControlLabel value="HML" control={<Radio />} label="HML" />
+                <FormControlLabel value="PRD" control={<Radio />} label="PRD" />
+              </RadioGroup>
+            </Box>
+            <Button variant="contained" onClick={handleSubmit}>
+              Enviar
+            </Button>
+          </FormGroup>
         </Box>
       </Modal>
     </>
--- pages/FormAdd/styles.ts
+++ pages/FormAdd/styles.ts
@@ -5,7 +5,8 @@ export const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #000",
+  border: "1px solid #000",
   boxShadow: 24,
   p: 4,
+  borderRadius: "0.5rem",
 };
